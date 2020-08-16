c88455dcde937bbedabb83af60fbcd905c58cc71
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-08-16 10:01:05 +0200

    list questionBank with Question class created

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,18 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questionBank = [
+    Question(
+        question: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        question: 'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(
+      question: 'A slug\'s blood is green.',
+      answer: true,
+    ),
   ];
-
-  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
