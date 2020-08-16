dc049a1ad562942510da668480f51259a09d642d
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-08-16 15:49:27 +0200

    questionBank added to classs QUizBrain

--- lib/main.dart
+++ lib/main.dart
@@ -28,19 +28,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<Question> questionBank = [
-    Question(
-        question: 'You can lead a cow down stairs but not up stairs.',
-        answer: false),
-    Question(
-        question: 'Approximately one quarter of human bones are in the feet.',
-        answer: true),
-    Question(
-      question: 'A slug\'s blood is green.',
-      answer: true,
-    ),
-  ];
-
   int questionNumber = 0;
 
   @override
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,1 +1,14 @@
-class QuizBrain {}
+class QuizBrain {
+  List<Question> questionBank = [
+    Question(
+        question: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        question: 'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(
+      question: 'A slug\'s blood is green.',
+      answer: true,
+    ),
+  ];
+}
