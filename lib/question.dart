59ec172040d5f9838bab1840980b5d6442e28b20
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-08-16 09:53:14 +0200

    class constructor created

--- lib/question.dart
+++ lib/question.dart
@@ -1,1 +1,9 @@
-class Question {}
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question({String question, bool answer}) {
+    questionText = question;
+    questionAnswer = answer;
+  }
+}
