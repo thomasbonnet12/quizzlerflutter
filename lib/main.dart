01de2196f85c41489fe13ca7fb296f7794f02c70
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-08-17 17:45:38 +0200

    .clear added to scorekeeper to clean it and scorekeeper.add if statement moved on the bottom to avoid a close or a check icon to stay after the end of the quiz

--- lib/main.dart
+++ lib/main.dart
@@ -34,22 +34,6 @@ class _QuizPageState extends State<QuizPage> {
 
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getAnswer();
-    //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
-    if (quizBrain.isFinished() == true) {
-      Alert(
-              context: context,
-              title: "Finished!",
-              desc: "You\'ve reached the end of the quiz.")
-          .show();
-      quizBrain.reset();
-      scoreKeeper = [];
-    }
-    //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
-    //HINT! Step 4 Part B is in the quiz_brain.dart
-    //TODO: Step 4 Part C - reset the questionNumber,
-    //TODO: Step 4 Part D - empty out the scoreKeeper.
-
-    //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps
 
     if (userPickedAnswer == correctAnswer) {
       scoreKeeper.add(
@@ -60,6 +44,24 @@ class _QuizPageState extends State<QuizPage> {
         Icon(Icons.close, color: Colors.red),
       );
     }
+
+    //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+    if (quizBrain.isFinished() == true) {
+      Alert(
+              context: context,
+              title: "Finished!",
+              desc: "You\'ve reached the end of the quiz.")
+          .show();
+      quizBrain.reset();
+      scoreKeeper.clear();
+    }
+    //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+    //HINT! Step 4 Part B is in the quiz_brain.dart
+    //TODO: Step 4 Part C - reset the questionNumber,
+    //TODO: Step 4 Part D - empty out the scoreKeeper.
+
+    //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps
+
     setState(() {
       quizBrain.nextQuestion();
     });
